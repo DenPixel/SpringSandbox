366e9e6622b22409eb9d84af49ad84d81bcd0f05
Pixel <dev.den.bond@gmail.com>
2021-03-13 01:19:34 +0200

    added two fields

--- src/main/webapp/java/org/pixel/springsandbox/beans/GameConsole.java
+++ src/main/webapp/java/org/pixel/springsandbox/beans/GameConsole.java
@@ -2,14 +2,17 @@ package org.pixel.springsandbox.beans;
 
 public class GameConsole {
     private AbstractGame game;
+    private String name;
+    private int RAM;
 
-    public GameConsole() {}
+    public GameConsole() {
+    }
 
     public GameConsole(AbstractGame game) {
         this.game = game;
     }
 
-    public void playGame(){
+    public void playGame() {
         game.apply();
     }
 
@@ -19,5 +22,21 @@ public class GameConsole {
 
     public void setGame(AbstractGame game) {
         this.game = game;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRAM() {
+        return RAM;
+    }
+
+    public void setRAM(int RAM) {
+        this.RAM = RAM;
     }
 }
