53a66f4bc3f22901723c1ec7aba501eec0200c26
DenPixel <dev.den.bond@gmail.com>
2021-03-29 16:04:11 +0300

    creat calculator page

--- src/main/java/org/pixel/springsandbox/contoller/MainController.java
+++ src/main/java/org/pixel/springsandbox/contoller/MainController.java
@@ -1,18 +1,52 @@
 package org.pixel.springsandbox.contoller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage() {
         return "main/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage(){
+    public String goodbyePage() {
         return "main/goodbye";
+    }
+
+
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam("a") int a,
+                                 @RequestParam("b") int b,
+                                 @RequestParam("action") String action,
+                                 Model model) {
+
+        String message;
+
+        switch (action) {
+            case "multi":
+                message = a + " * " + b + " = " + (a * b);
+                break;
+            case "add":
+                message = a + "+ " + b + " = " + (a + b);
+                break;
+            case "sub":
+                message = a + " - " + b + " = " + (a * b);
+                break;
+            case "div":
+                message = a + " / " + b + " = " +  ((float) a / b);
+                break;
+            default:
+                message = "action not found";
+        }
+
+
+        model.addAttribute("message", message);
+
+        return "main/calculator";
     }
 }
