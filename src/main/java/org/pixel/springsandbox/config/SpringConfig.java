6e715852085af4322d89c01817876f553d407337
DenPixel <dev.den.bond@gmail.com>
2021-04-05 23:49:14 +0300

    JDBC and postgres PersonDAO changed to work with a database using JDBC Added DB properties Added postgres dependency

--- src/main/java/org/pixel/springsandbox/config/SpringConfig.java
+++ src/main/java/org/pixel/springsandbox/config/SpringConfig.java
@@ -5,12 +5,19 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan("org.pixel.springsandbox")
@@ -22,6 +29,33 @@ public class SpringConfig implements WebMvcConfigurer {
     @Autowired
     public SpringConfig(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public Connection dbConnection() {
+        Connection connection = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            Properties props = new Properties();
+            File file = ResourceUtils.getFile("classpath:DB.properties");
+            InputStream in = new FileInputStream(file);
+
+            props.load(in);
+
+            connection = DriverManager.getConnection(props.getProperty("url"), props);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
     }
 
     @Bean
--- src/main/java/org/pixel/springsandbox/dao/PersonDAO.java
+++ src/main/java/org/pixel/springsandbox/dao/PersonDAO.java
@@ -1,48 +1,107 @@
 package org.pixel.springsandbox.dao;
 
 import org.pixel.springsandbox.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
 
-    private static int COUNT_PEOPLE;
-    private final List<Person> people;
+    private final Connection connection;
 
-    {
-        people = new ArrayList<>();
-
-        people.add(new Person(++COUNT_PEOPLE, "qwerty", "qw@wq.com"));
-        people.add(new Person(++COUNT_PEOPLE, "asdfgh", "as@sa.com"));
-        people.add(new Person(++COUNT_PEOPLE, "zxcvbn", "zx@xz.com"));
+    @Autowired
+    public PersonDAO(Connection connection) {
+        this.connection = connection;
     }
 
     public List<Person> getPeople() {
+        String SQL = "SELECT * FROM person";
+        List<Person> people = new ArrayList<>();
+
+        try (final PreparedStatement ps = connection.prepareStatement(SQL)) {
+            try (final ResultSet peopleResultSet = ps.executeQuery()) {
+                while (peopleResultSet.next()) {
+                    int id = peopleResultSet.getInt("id");
+                    String nickname = peopleResultSet.getString("nickname");
+                    String email = peopleResultSet.getString("email");
+
+                    people.add(new Person(id, nickname, email));
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
         return people;
     }
 
     public Person getPerson(int id) {
-        return getPeople().stream()
-                .filter(person -> person.getId() == id)
-                .findAny()
-                .orElseThrow();
+        Person person = null;
+
+        try (final PreparedStatement statement = connection.prepareStatement(
+                "SELECT nickname, email FROM person WHERE id=?"
+        )) {
+            statement.setInt(1, id);
+            try (final ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    String nickname = resultSet.getString("nickname");
+                    String email = resultSet.getString("email");
+
+                    person = new Person(id, nickname, email);
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return person;
     }
 
     public void save(Person person) {
-        person.setId(++COUNT_PEOPLE);
-        people.add(person);
+        String SQL = "INSERT INTO person VALUES(default, ?, ?)";
+
+        try (final PreparedStatement statement = connection.prepareStatement(SQL)) {
+            statement.setString(1, person.getNickname());
+            statement.setString(2, person.getEmail());
+
+            statement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
     }
 
     public void update(int id, Person person) {
-        Person personForUp = getPerson(id);
-        personForUp.setEmail(person.getEmail());
-        personForUp.setNickname(person.getNickname());
+        String SQL = "UPDATE person SET nickname = ?, email = ? WHERE id = ?";
+
+        try (final PreparedStatement statement = connection.prepareStatement(SQL)) {
+            statement.setString(1, person.getNickname());
+            statement.setString(2, person.getEmail());
+            statement.setInt(3, id);
+
+            statement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void delete(int id) {
-        people.removeIf(person -> person.getId() == id);
+        String SQL = "DELETE FROM person WHERE id = ?";
+
+        try (final PreparedStatement statement = connection.prepareStatement(SQL)) {
+            statement.setInt(1, id);
+
+            statement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
